88de030955af23307834c1ec04c7be0a8632b9c2
Characin.choi <choisc91@naver.com>
2025-06-13 18:03:07 +0900

    Fix toolbar color and apply Material3 opt-in

--- app/src/main/java/kr/com/characin/codexexample/MainActivity.kt
+++ app/src/main/java/kr/com/characin/codexexample/MainActivity.kt
@@ -1,3 +1,4 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
 package kr.com.characin.codexexample
 
 import android.os.Bundle
@@ -13,6 +14,7 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
